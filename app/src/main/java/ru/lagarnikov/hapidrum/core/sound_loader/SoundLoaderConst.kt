ab02b9bcc4c81201f1be26440732d826785054e7
lagarnikov <m.lagarnikov@cft.ru>
2021-02-14 12:54:07 +0700

    - поменял формат сэмплов для глюкофонии на мп3 - почему то на многих девайсах с wav воспроизводилось ужасно, с шипением - поправил багу с отображением музыкальног строя для unison_of_hearts

--- app/src/main/java/ru/lagarnikov/hapidrum/core/sound_loader/SoundLoaderConst.kt
+++ app/src/main/java/ru/lagarnikov/hapidrum/core/sound_loader/SoundLoaderConst.kt
@@ -26,14 +26,14 @@ const val H_LOCAL = "h_MAIN_INSTRUMENT"
 const val ZERO_LOCAL = "zero_MAIN_INSTRUMENT"
 
 const val UNISON_OF_HEARTS_ = "unison_of_hearts_instrument"
-const val UNISON_OF_HEARTS_A_ = "unison_of_hearts_a.wav"
-const val UNISON_OF_HEARTS_B_ = "unison_of_hearts_b.wav"
-const val UNISON_OF_HEARTS_C_ = "unison_of_hearts_c.wav"
-const val UNISON_OF_HEARTS_D_ = "unison_of_hearts_d.wav"
-const val UNISON_OF_HEARTS_E_ = "unison_of_hearts_e.wav"
-const val UNISON_OF_HEARTS_F_ = "unison_of_hearts_f.wav"
-const val UNISON_OF_HEARTS_G_ = "unison_of_hearts_g.wav"
-const val UNISON_OF_HEARTS_H_ = "unison_of_hearts_h.wav"
+const val UNISON_OF_HEARTS_A_ = "unison_of_hearts_a.mp3"
+const val UNISON_OF_HEARTS_B_ = "unison_of_hearts_b.mp3"
+const val UNISON_OF_HEARTS_C_ = "unison_of_hearts_c.mp3"
+const val UNISON_OF_HEARTS_D_ = "unison_of_hearts_d.mp3"
+const val UNISON_OF_HEARTS_E_ = "unison_of_hearts_e.mp3"
+const val UNISON_OF_HEARTS_F_ = "unison_of_hearts_f.mp3"
+const val UNISON_OF_HEARTS_G_ = "unison_of_hearts_g.mp3"
+const val UNISON_OF_HEARTS_H_ = "unison_of_hearts_h.mp3"
 
 const val UNISON_OF_HEARTS_A_LOCAL = "a_UNISON_OF_HEARTS_"
 const val UNISON_OF_HEARTS_B_LOCAL = "b_UNISON_OF_HEARTS_"
@@ -45,14 +45,14 @@ const val UNISON_OF_HEARTS_G_LOCAL = "g_UNISON_OF_HEARTS_"
 const val UNISON_OF_HEARTS_H_LOCAL = "h_UNISON_OF_HEARTS_"
 
 const val UNISON_OF_HEARTS_FELT_ = "unison_of_hearts_felt_instrument"
-const val UNISON_OF_HEARTS_FELT_A_ = "unison_of_hearts_felt_a.wav"
-const val UNISON_OF_HEARTS_FELT_B_ = "unison_of_hearts_felt_b.wav"
-const val UNISON_OF_HEARTS_FELT_C_ = "unison_of_hearts_felt_c.wav"
-const val UNISON_OF_HEARTS_FELT_D_ = "unison_of_hearts_felt_d.wav"
-const val UNISON_OF_HEARTS_FELT_E_ = "unison_of_hearts_felt_e.wav"
-const val UNISON_OF_HEARTS_FELT_F_ = "unison_of_hearts_felt_f.wav"
-const val UNISON_OF_HEARTS_FELT_G_ = "unison_of_hearts_felt_g.wav"
-const val UNISON_OF_HEARTS_FELT_H_ = "unison_of_hearts_felt_h.wav"
+const val UNISON_OF_HEARTS_FELT_A_ = "unison_of_hearts_felt_a.mp3"
+const val UNISON_OF_HEARTS_FELT_B_ = "unison_of_hearts_felt_b.mp3"
+const val UNISON_OF_HEARTS_FELT_C_ = "unison_of_hearts_felt_c.mp3"
+const val UNISON_OF_HEARTS_FELT_D_ = "unison_of_hearts_felt_d.mp3"
+const val UNISON_OF_HEARTS_FELT_E_ = "unison_of_hearts_felt_e.mp3"
+const val UNISON_OF_HEARTS_FELT_F_ = "unison_of_hearts_felt_f.mp3"
+const val UNISON_OF_HEARTS_FELT_G_ = "unison_of_hearts_felt_g.mp3"
+const val UNISON_OF_HEARTS_FELT_H_ = "unison_of_hearts_felt_h.mp3"
 
 const val UNISON_OF_HEARTS_FELT_A_LOCAL = "a_UNISON_OF_HEARTS_felt_"
 const val UNISON_OF_HEARTS_FELT_B_LOCAL = "b_UNISON_OF_HEARTS_felt_"
@@ -64,14 +64,14 @@ const val UNISON_OF_HEARTS_FELT_G_LOCAL = "g_UNISON_OF_HEARTS_felt_"
 const val UNISON_OF_HEARTS_FELT_H_LOCAL = "h_UNISON_OF_HEARTS_felt_"
 
 const val OPEN_SPACE_ = "open_space_instrument"
-const val OPEN_SPACE_A_ = "open_space_a.wav"
-const val OPEN_SPACE_B_ = "open_space_b.wav"
-const val OPEN_SPACE_C_ = "open_space_c.wav"
-const val OPEN_SPACE_D_ = "open_space_d.wav"
-const val OPEN_SPACE_E_ = "open_space_e.wav"
-const val OPEN_SPACE_F_ = "open_space_f.wav"
-const val OPEN_SPACE_G_ = "open_space_g.wav"
-const val OPEN_SPACE_H_ = "open_space_h.wav"
+const val OPEN_SPACE_A_ = "open_space_a.mp3"
+const val OPEN_SPACE_B_ = "open_space_b.mp3"
+const val OPEN_SPACE_C_ = "open_space_c.mp3"
+const val OPEN_SPACE_D_ = "open_space_d.mp3"
+const val OPEN_SPACE_E_ = "open_space_e.mp3"
+const val OPEN_SPACE_F_ = "open_space_f.mp3"
+const val OPEN_SPACE_G_ = "open_space_g.mp3"
+const val OPEN_SPACE_H_ = "open_space_h.mp3"
 
 const val OPEN_SPACE_A_LOCAL = "a_OPEN_SPACE_"
 const val OPEN_SPACE_B_LOCAL = "b_OPEN_SPACE_"
@@ -83,14 +83,14 @@ const val OPEN_SPACE_G_LOCAL = "g_OPEN_SPACE_"
 const val OPEN_SPACE_H_LOCAL = "h_OPEN_SPACE_"
 
 const val OPEN_SPACE_FELT_ = "open_space_felt_instrument"
-const val OPEN_SPACE_FELT_A_ = "open_space_felt_a.wav"
-const val OPEN_SPACE_FELT_B_ = "open_space_felt_b.wav"
-const val OPEN_SPACE_FELT_C_ = "open_space_felt_c.wav"
-const val OPEN_SPACE_FELT_D_ = "open_space_felt_d.wav"
-const val OPEN_SPACE_FELT_E_ = "open_space_felt_e.wav"
-const val OPEN_SPACE_FELT_F_ = "open_space_felt_f.wav"
-const val OPEN_SPACE_FELT_G_ = "open_space_felt_g.wav"
-const val OPEN_SPACE_FELT_H_ = "open_space_felt_h.wav"
+const val OPEN_SPACE_FELT_A_ = "open_space_felt_a.mp3"
+const val OPEN_SPACE_FELT_B_ = "open_space_felt_b.mp3"
+const val OPEN_SPACE_FELT_C_ = "open_space_felt_c.mp3"
+const val OPEN_SPACE_FELT_D_ = "open_space_felt_d.mp3"
+const val OPEN_SPACE_FELT_E_ = "open_space_felt_e.mp3"
+const val OPEN_SPACE_FELT_F_ = "open_space_felt_f.mp3"
+const val OPEN_SPACE_FELT_G_ = "open_space_felt_g.mp3"
+const val OPEN_SPACE_FELT_H_ = "open_space_felt_h.mp3"
 
 const val OPEN_SPACE_FELT_A_LOCAL = "a_OPEN_SPACE_felt_"
 const val OPEN_SPACE_FELT_B_LOCAL = "b_OPEN_SPACE_felt_"
@@ -114,14 +114,14 @@ val MAIN_SOUNDS_ = arrayListOf<InstrumentSound>(
 )
 
 val UNISON_OF_HEARTS_SOUNDS_ = arrayListOf<InstrumentSound>(
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_A_, UNISON_OF_HEARTS_A_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_B_, UNISON_OF_HEARTS_B_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_C_, UNISON_OF_HEARTS_C_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_D_, UNISON_OF_HEARTS_D_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_E_, UNISON_OF_HEARTS_E_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_F_, UNISON_OF_HEARTS_F_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_G_, UNISON_OF_HEARTS_G_LOCAL, EXT_WAV),
-    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_H_, UNISON_OF_HEARTS_H_LOCAL, EXT_WAV)
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_A_, UNISON_OF_HEARTS_A_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_B_, UNISON_OF_HEARTS_B_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_C_, UNISON_OF_HEARTS_C_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_D_, UNISON_OF_HEARTS_D_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_E_, UNISON_OF_HEARTS_E_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_F_, UNISON_OF_HEARTS_F_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_G_, UNISON_OF_HEARTS_G_LOCAL, EXT_MP3),
+    InstrumentSound(UNISON_OF_HEARTS_, UNISON_OF_HEARTS_H_, UNISON_OF_HEARTS_H_LOCAL, EXT_MP3)
 )
 
 val UNISON_OF_HEARTS_SOUNDS_FELT_ = arrayListOf<InstrumentSound>(
@@ -129,72 +129,72 @@ val UNISON_OF_HEARTS_SOUNDS_FELT_ = arrayListOf<InstrumentSound>(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_A_,
         UNISON_OF_HEARTS_FELT_A_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_B_,
         UNISON_OF_HEARTS_FELT_B_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_C_,
         UNISON_OF_HEARTS_FELT_C_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_D_,
         UNISON_OF_HEARTS_FELT_D_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_E_,
         UNISON_OF_HEARTS_FELT_E_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_F_,
         UNISON_OF_HEARTS_FELT_F_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_G_,
         UNISON_OF_HEARTS_FELT_G_LOCAL,
-        EXT_WAV
+        EXT_MP3
     ),
     InstrumentSound(
         UNISON_OF_HEARTS_FELT_,
         UNISON_OF_HEARTS_FELT_H_,
         UNISON_OF_HEARTS_FELT_H_LOCAL,
-        EXT_WAV
+        EXT_MP3
     )
 )
 
 val OPEN_SPACE_SOUNDS_ = arrayListOf<InstrumentSound>(
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_A_, OPEN_SPACE_A_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_B_, OPEN_SPACE_B_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_C_, OPEN_SPACE_C_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_D_, OPEN_SPACE_D_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_E_, OPEN_SPACE_E_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_F_, OPEN_SPACE_F_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_G_, OPEN_SPACE_G_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_H_, OPEN_SPACE_H_LOCAL, EXT_WAV)
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_A_, OPEN_SPACE_A_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_B_, OPEN_SPACE_B_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_C_, OPEN_SPACE_C_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_D_, OPEN_SPACE_D_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_E_, OPEN_SPACE_E_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_F_, OPEN_SPACE_F_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_G_, OPEN_SPACE_G_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_, OPEN_SPACE_H_, OPEN_SPACE_H_LOCAL, EXT_MP3)
 )
 
 val OPEN_SPACE_SOUNDS_FELT_ = arrayListOf<InstrumentSound>(
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_A_, OPEN_SPACE_FELT_A_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_B_, OPEN_SPACE_FELT_B_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_C_, OPEN_SPACE_FELT_C_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_D_, OPEN_SPACE_FELT_D_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_E_, OPEN_SPACE_FELT_E_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_F_, OPEN_SPACE_FELT_F_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_G_, OPEN_SPACE_FELT_G_LOCAL, EXT_WAV),
-    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_H_, OPEN_SPACE_FELT_H_LOCAL, EXT_WAV)
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_A_, OPEN_SPACE_FELT_A_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_B_, OPEN_SPACE_FELT_B_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_C_, OPEN_SPACE_FELT_C_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_D_, OPEN_SPACE_FELT_D_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_E_, OPEN_SPACE_FELT_E_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_F_, OPEN_SPACE_FELT_F_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_G_, OPEN_SPACE_FELT_G_LOCAL, EXT_MP3),
+    InstrumentSound(OPEN_SPACE_FELT_, OPEN_SPACE_FELT_H_, OPEN_SPACE_FELT_H_LOCAL, EXT_MP3)
 )
 
 enum class Instruments(val sounds: ArrayList<InstrumentSound>) {
